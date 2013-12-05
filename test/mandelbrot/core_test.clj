a9c9d57d1333ca129e422a79106200be29b6b985
Chad Heim <chad.m.heim@gmail.com>
2013-12-04 23:41:48 -0600

    removed unused test

--- test/mandelbrot/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns mandelbrot-clojure.core-test
-  (:require [clojure.test :refer :all]
-            [mandelbrot-clojure.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
